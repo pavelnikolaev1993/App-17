75f809bd0fc1040cfa3f71bdca0b349f04fa1c47
НиколаевПА <@NIKOLAEV>
2022-02-14 11:12:27 +0300

    Добавьте файлы проекта.

---
+++ Bank.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App17
+{
+    class Bank<T>
+    {
+    public T Number { get; set; }
+    public int Balance { get; set; }
+    public string? Name { get; set; }
+
+        //public Bank(T num, string name, int balance)
+        //{
+        //    Number = num;
+        //    Name = name;
+        //    Balance = balance;
+        //}
+
+        public string GetInfo()
+        {
+            return $"{Number} {Name} {Balance}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number =(T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите Баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+        }
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using App17;
+//Console.WriteLine("Введите числовой номер счета");
+//int numInt = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите ФИО");
+//string name = Console.ReadLine();
+//Console.WriteLine("Введите Баланс");
+//int balance = Convert.ToInt32(Console.ReadLine());
+
+//Bank<int> bank1 = new Bank<int>(numInt, name,balance );
+//Console.WriteLine(bank1.GetInfo());
+
+//Console.WriteLine("Введите строковый номер счета");
+//string numString = Console.ReadLine();
+//Console.WriteLine("Введите ФИО");
+//name = Console.ReadLine();
+//Console.WriteLine("Введите Баланс");
+//balance = Convert.ToInt32(Console.ReadLine());
+
+//Bank<string> bank2 = new Bank<string>( numString, name,balance);
+//Console.WriteLine(bank2.GetInfo());
+Bank<int> bank1 = new Bank<int>();
+bank1.Input();
+Console.WriteLine(bank1.GetInfo());
+
+Bank<string> bank2 = new Bank<string>();
+bank2.Input();
+Console.WriteLine(bank2.GetInfo());
+
+
+
+
+
